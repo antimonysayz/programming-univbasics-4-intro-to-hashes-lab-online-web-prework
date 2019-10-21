6529c14dc00f2cd9c0ac23c15a88a05244590d38
Anthony Mai <anthony.mai530@gmail.com>
2019-10-21 22:15:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] 
-    return hash[key] += 1 
+  if #{hash}[#{key}] 
+    return #{hash}[#{key}] += 1 
   else 
     #{hash}[#{key}] = 1
   end
